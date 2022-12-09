c7cb93d7358c3cd5748a31f8cc038502e786e979
rahulhencha <70096364+rahulhencha@users.noreply.github.com>
2022-12-10 00:22:59 +0530

    add fonts

---
+++ index1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>AJMAL || HOME</title>
+    <?php include('include/head-link.php'); ?>
+    <link rel="stylesheet" href="css/slick.css" type="text/css">
+    <link rel="stylesheet" href="css/slick-theme.css" type="text/css">
+</head>
+
+<body>
+    <?php include('include/header.php'); ?>
+    <main class="mainContent homePage">
+        <section class="fragrancesSection">
+            <div class="container">
+            <h1 class=" font-smd">hello</h1>
+            </div>
+            
+        </section>
+
+    </main>
+    <?php include ('include/footer.php') ?>
+    <?php include ('include/footer-scripts.php') ?>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+   
+    <script>
+        
+    </script>
+</body>
+
+</html>
