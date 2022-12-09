738cac1b762c131bb63ac8fd29e43b3f4a9b91c8
devesh gour <deveshgour98@gmail.com>
2022-12-09 23:29:28 +0530

    update html

---
+++ include/footer-scripts.php
@@ -0,0 +1,23 @@
+<!-- jquery -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+
+<!-- popper js -->
+<script type="text/javascript" src="js/popper.min.js"></script>
+
+<!-- bootsrtrap -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+<script>
+    //add and remove class in header on scroll to make header small
+//     $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//     if (scroll >= 50) {
+//         $("#userHeader").addClass("smallHeader");
+//         $("body").addClass("stickyHeader");
+//     } else {
+//         $("#userHeader").removeClass("smallHeader");
+//         $("body").removeClass("stickyHeader");
+//     }
+// });
+</script>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>AJMAL || HOME</title>
+    <?php include('include/head-link.php'); ?>
+    <link rel="stylesheet" href="css/slick.css" type="text/css">
+    <link rel="stylesheet" href="css/slick-theme.css" type="text/css">
+</head>
+
+<body>
+    <?php include('include/header.php'); ?>
+    <main class="mainContent homePage">
+   
+
+    </main>
+    <?php include ('include/footer.php') ?>
+    <?php include ('include/footer-scripts.php') ?>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+   
+    <script>
+  
+    
+    //   $('.bannerSlider').slick({
+    //         centerMode: true,
+    //         centerPadding: '90px',
+    //         slidesToShow: 1,
+    //         arrows: false,
+    //         dots:true,
+    //         responsive: [
+    //             {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: false,
+    //                 centerMode: true,
+    //                     centerPadding: '40px',
+    //                     slidesToShow: 3
+    //                 }
+    //                 },
+    //                 {
+    //                 breakpoint: 480,
+    //                 settings: {
+    //                     arrows: false,
+    //                     centerMode: true,
+    //                     centerPadding: '40px',
+    //                     slidesToShow: 1
+    //                 }
+    //                 }
+    //             ]
+    //             });
+
+             
+        
+    </script>
+</body>
+
+</html>
