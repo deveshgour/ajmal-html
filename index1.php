832c4b7f48b7bcd19517e20a56270df7721b5734
rahulhencha <70096364+rahulhencha@users.noreply.github.com>
2022-12-10 08:55:30 +0530

    worked on home

--- index1.php
+++ index1.php
@@ -12,12 +12,45 @@
 <body>
     <?php include('include/header.php'); ?>
     <main class="mainContent homePage">
-        <section class="fragrancesSection">
+        <section class="fragrancesSection py-55">
             <div class="container">
-            <h1 class=" font-smd">hello</h1>
+                <div class="fragrancesSection_bg position-relative">
+                    <div class="fragrancesSection_inner  text-center">
+                        <h3 class="font-grg">At Ajmal Perfumes, the art of perfumery is crafted into a science of everlasting memories through their fragrances</h3>
+                        <a href="javascript:void(0);" class="discoverAllBtn">Discover all</a>
+                    </div>
+                </div>
             </div>
-            
         </section>
+        <Section class="arrivalsSection py-55 pt-0">
+            <div class="container">
+                <div class="arrivalsSection_top d-flex align-items-center justify-content-between">
+                    <h3 class="sectionHeading mb-0">New arrivals</h3>
+                    <a href="javascript:void(0);" class="readMoreBtn">Discover all</a>
+                </div>
+                <div class="arrivalsSection_bottom">
+                    <div class="productCard">
+                        <div class="productCard_inner productCard_inner--light">
+                            <div class="productCard_top d-flex align-item-start justify-content-between">
+                                <a href="javascript:void(0);" class="productBadge"></a>
+                                <img src="images/product-1.png"  class="img-fluid" alt="product-img">
+                                <a href="javascript:void(0);" class="productLike"></a>
+                            </div>
+                            <div class="productCard_center text-center">
+                                <span class="offer">50%</span>
+                                <ul class="category d-flex list-unstyled justify-content-center mb-0">
+                                    <li>OIL PARFUM</li>
+                                    <li>75ML</li>
+                                    <li>WOMEN</li>
+                                </ul>
+                                <h3>Jannatul Fridaus</h3>
+                                <span class="price">12 AED <del>24 AED</del></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Section>
 
     </main>
     <?php include ('include/footer.php') ?>
