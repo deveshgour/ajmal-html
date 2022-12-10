178287f67d7a5ef09e26b9bf6c65c16ffbb90f0d
devesh gour <deveshgour98@gmail.com>
2022-12-10 13:59:09 +0530

    work on header

---
+++ include/header.php
@@ -0,0 +1,88 @@
+<header class="header afterLogin" id="userHeader">
+   
+        <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/logo.svg" alt="logo" class="img-fluid whiteLogo">
+                <!-- <img src="images/logo.svg" alt="logo" class="img-fluid blackLogo"> -->
+            </a>
+        
+            <button class="navbar-toggler order-12" id="mobileMenuBar" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav align-items-lg-center mb-0 m-auto" id="navbarToggle">
+                    <li class="nav-item dropdown dropdown--custom">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Perfumes
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="product.php">option</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">Perfume oils</a>
+                    </li>
+                    <li class="nav-item dropdown dropdown--custom">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Agarwood
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">option</a>
+                       
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">Oud</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">Collections</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">Gifts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">Sale</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">About us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="navbar-right d-flex align-items-center">
+                <ul class="navbar-nav align-items-lg-center mb-0 ml-auto">
+                    <li class="nav-item country-menu">
+                        <a class="nav-link" href="javascript:void(0);">
+                            <img src="images/country1.png" alt="country"/> 
+                            <span class="nav-text"> العربية</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">
+                            <img src="images/logout.svg" alt="logout"/> 
+                        </a>
+                    </li>
+                    <li class="nav-item cart-menu">
+                        <a class="nav-link" href="javascript:void(0);">
+                            <img src="images/cart.svg" alt="cart"/> 
+                            <span class="nav-text-count">2</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">
+                            <img src="images/ven.svg" alt="ven"/> 
+                        </a>
+                    </li>
+
+                    
+                </ul>
+            </div>
+            </div>
+        </nav>
+  
+</header>
