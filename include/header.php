350d718cc95c823a4661fc9cf9fa36901f2bdb9c
devesh gour <deveshgour98@gmail.com>
2022-12-13 23:54:43 +0530

    update html

--- include/header.php
+++ include/header.php
@@ -25,7 +25,7 @@
                 </form>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav align-items-lg-center mb-0 m-auto" id="navbarToggle">
-                        <li class="nav-item dropdown dropdown--custom">
+                        <li class="nav-item dropdown">
                             <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
                                 Perfumes
                             </a>
@@ -35,17 +35,19 @@
                                         <div class="row">
                                             <div class="col-md-3">
                                                 <ul class="list-unstyled">
-                                                    <li><a class="dropdown-item" href="javascript:;">Egift Card</a></li>
-                                                    <li><a class="dropdown-item" href="javascript:;">For Him</a></li>
-                                                    <li><a class="dropdown-item" href="javascript:;">For Her</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Online Exclusive</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Customised Bundles</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Home Fragrance</a></li>
                                                 </ul>
                                             </div>
                                             <div class="col-md-3">
+                                                <h4 class="dropdown-menu-title">By Fragrance</h4>
                                                 <ul class="list-unstyled">
-                                                    <li><a class="dropdown-item" href="javascript:;">Party</a></li>
-                                                    <li><a class="dropdown-item" href="javascript:;">Anniversary</a></li>
-                                                    <li><a class="dropdown-item" href="javascript:;">Outing</a></li>
-                                                    <li><a class="dropdown-item" href="javascript:;">Long Drive</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Woody</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Rose</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Sweet</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Fresh</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Oudh</a></li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -81,8 +83,51 @@
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);">Oud</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="javascript:void(0);">Collections</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Collections
+                            </a>
+                            <div class="dropdown-menu">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <ul class="list-unstyled">
+                                                    <li><a class="dropdown-item" href="javascript:;">Egift Card</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">For Him</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">For Her</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <h4 class="dropdown-menu-title">By Occasion</h4>
+                                                <ul class="list-unstyled">
+                                                    <li><a class="dropdown-item" href="javascript:;">Party</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Anniversary</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Outing</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Long Drive</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                               
+                                    <div class="col-md-5">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                 <div class="menuImg">
+                                                    <a href="#"><img src="images/menu-img-1.jpg" class="img-fluid"/></a>
+                                                    <span class="menuImg_txt">Jannatual Firdaus</span>
+                                                 </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                 <div class="menuImg">
+                                                    <a href="#"><img src="images/menu-img-2.jpg" class="img-fluid"/></a>
+                                                    <span class="menuImg_txt">Jannatual Firdaus</span>
+                                                 </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);">Gifts</a>
