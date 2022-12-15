a93fc715fd48e4d7db38d262abff882943ab35e1
devesh gour <deveshgour98@gmail.com>
2022-12-15 22:13:15 +0530

    all image compressed

--- include/header.php
+++ include/header.php
@@ -73,10 +73,56 @@
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);">Perfume oils</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Agarwood
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                                AGARWOOD
                             </a>
+                            <div class="dropdown-menu">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <a href="#">
+                                                    <div class="dropdown-menu-cat">Hindi</div>
+                                                    <div class="dropdown-menu-img">
+                                                        <img src="images/hindi.png" alt="hindi" class="img-fluid"/>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <a href="#">
+                                                    <div class="dropdown-menu-cat">Muri</div>
+                                                    <div class="dropdown-menu-img">
+                                                        <img src="images/muri.png" alt="muri" class="img-fluid"/>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <a href="#">
+                                                    <div class="dropdown-menu-cat">Indian</div>
+                                                    <div class="dropdown-menu-img">
+                                                        <img src="images/indian.png" alt="indian" class="img-fluid"/>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="dropdown-menu-cat">Best Seller</div>
+                                                <div class="menuImg">
+                                                    <a href="#"><img src="images/menu-bestseller.png" class="img-fluid" /></a>
+                                                    <span class="menuImg_txt">Jannatual Firdaus</span>
+                                                </div>
+                                            </div>
+                                         
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);">Oud</a>
@@ -130,8 +176,62 @@
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);">Gifts</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="javascript:void(0);">Sale</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Sale
+                            </a>
+                            <div class="dropdown-menu">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <ul class="list-unstyled">
+                                                    <li><a class="dropdown-item" href="javascript:;">Men</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Woman</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Unisex</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Oman</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <h4 class="dropdown-menu-title">By Occasion</h4>
+                                                <ul class="list-unstyled">
+                                                    <li><a class="dropdown-item" href="javascript:;">Party</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Anniversary</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Outing</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Long Drive</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <h4 class="dropdown-menu-title">By Occasion</h4>
+                                                <ul class="list-unstyled">
+                                                    <li><a class="dropdown-item" href="javascript:;">Woody</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Rose</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Sweet</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Fresh</a></li>
+                                                    <li><a class="dropdown-item" href="javascript:;">Oudh</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-5">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="menuImg">
+                                                    <a href="#"><img src="images/sale_img.jpg" class="img-fluid" /></a>
+                                                    <span class="menuImg_txt">Jannatual Firdaus</span>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="menuImg">
+                                                    <a href="#"><img src="images/sale_img2.jpg" class="img-fluid" /></a>
+                                                    <span class="menuImg_txt">Jannatual Firdaus</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);">About us</a>
@@ -150,7 +250,7 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="javascript:void(0);" data-toggle="modal" data-target="#loginMOdal">
-                            <img src="images/logout.svg" alt="logout"/> 
+                            <img src="images/logout.svg" alt="logout" />
                         </a>
                     </li>
                     <li class="nav-item cart-menu">
@@ -180,61 +280,64 @@
 
 <div class="countryMenu">
     <div class="countryMenu_header">
-    <a href="javascript:;" class="countryMenu_close"><img src="images/close.svg" alt="close"/></a>
-    <h2 class="countryMenu_title">Country</h2>
+        <a href="javascript:;" class="countryMenu_close"><img src="images/close.svg" alt="close" /></a>
+        <h2 class="countryMenu_title">Country</h2>
     </div>
     <div class="countryMenu_body">
-    <ul class="list-unstyled">
-        <li>
-            <a class="nav-link" href="javascript:void(0);">
-                <img src="images/country1.png" alt="country">
-                <span class="nav-text"> UAE (AED)</span>
-            </a>
-        </li>
-        <li>
-            <a class="nav-link" href="javascript:void(0);">
-                <img src="images/ksa.png" alt="country">
-                <span class="nav-text"> KSA (SAR)</span>
-            </a>
-        </li>
-        <li>
-            <a class="nav-link" href="javascript:void(0);">
-                <img src="images/kwd.png" alt="country">
-                <span class="nav-text"> Kuwait (KWD)</span>
-            </a>
-        </li>
-        <li>
-            <a class="nav-link" href="javascript:void(0);">
-                <img src="images/bhd.png" alt="country">
-                <span class="nav-text"> Bahrain (BHD)</span>
-            </a>
-        </li>
-        <li>
-            <a class="nav-link" href="javascript:void(0);">
-                <img src="images/qar.png" alt="country">
-                <span class="nav-text"> Quatar (QAR)</span>
-            </a>
-        </li>
-        <li>
-            <a class="nav-link" href="javascript:void(0);">
-                <img src="images/omr.png" alt="country">
-                <span class="nav-text"> Oman (OMR)</span>
-            </a>
-        </li>
-    </ul>
+        <ul class="list-unstyled">
+            <li>
+                <a class="nav-link" href="javascript:void(0);">
+                    <img src="images/country1.png" alt="country">
+                    <span class="nav-text"> UAE (AED)</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="javascript:void(0);">
+                    <img src="images/ksa.png" alt="country">
+                    <span class="nav-text"> KSA (SAR)</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="javascript:void(0);">
+                    <img src="images/kwd.png" alt="country">
+                    <span class="nav-text"> Kuwait (KWD)</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="javascript:void(0);">
+                    <img src="images/bhd.png" alt="country">
+                    <span class="nav-text"> Bahrain (BHD)</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="javascript:void(0);">
+                    <img src="images/qar.png" alt="country">
+                    <span class="nav-text"> Quatar (QAR)</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="javascript:void(0);">
+                    <img src="images/omr.png" alt="country">
+                    <span class="nav-text"> Oman (OMR)</span>
+                </a>
+            </li>
+        </ul>
     </div>
 </div>
 
 <!-- cart menu -->
 <div class="cartMenu">
     <div class="cartMenu_header">
-        <a href="javascript:;" class="cartMenu_close"><img src="images/close.svg" alt="close"/></a>
+        <a href="javascript:;" class="cartMenu_close"><img src="images/close.svg" alt="close" /></a>
         <h2 class="cartMenu_title">Your shopping bag (6)</h2>
 
         <div class="freeDelivery d-flex align-items-center justify-content-between">
             <div class="d-flex align-items-center">
-                <div class="freeDelivery_icon"><img src="images/free-delivery.svg" alt="free delivery"/></div>
-                <div class="freeDelivery_cnt"><h3>Free delivery</h3><p class="mb-0">On credit/debit cards order</p></div>
+                <div class="freeDelivery_icon"><img src="images/free-delivery.svg" alt="free delivery" /></div>
+                <div class="freeDelivery_cnt">
+                    <h3>Free delivery</h3>
+                    <p class="mb-0">On credit/debit cards order</p>
+                </div>
             </div>
             <div class="freeDelivery_action"><a href="#"><img src="images/vertical-menu.svg" alt="menu"></a></div>
         </div>
@@ -242,9 +345,9 @@
     <div class="cartMenu_body">
         <ul class="list-unstyled">
             <li class="cartBox d-flex align-items-center">
-                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid"/></a>
+                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid" /></a>
                 <div class="cartBox_img">
-                    <img src="images/cart-img.jpg" alt="product" class="img-fluid"/>
+                    <img src="images/cart-img.jpg" alt="product" class="img-fluid" />
                 </div>
                 <div class="cartBox_cnt">
                     <h4>1001 Nights</h4>
@@ -253,9 +356,9 @@
                 </div>
             </li>
             <li class="cartBox d-flex align-items-center">
-                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid"/></a>
+                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid" /></a>
                 <div class="cartBox_img">
-                    <img src="images/review-product.png" alt="product" class="img-fluid"/>
+                    <img src="images/review-product.png" alt="product" class="img-fluid" />
                 </div>
                 <div class="cartBox_cnt">
                     <h4>1001 Nights</h4>
@@ -264,9 +367,9 @@
                 </div>
             </li>
             <li class="cartBox d-flex align-items-center">
-                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid"/></a>
+                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid" /></a>
                 <div class="cartBox_img">
-                    <img src="images/cart-img.jpg" alt="product" class="img-fluid"/>
+                    <img src="images/cart-img.jpg" alt="product" class="img-fluid" />
                 </div>
                 <div class="cartBox_cnt">
                     <h4>1001 Nights</h4>
@@ -275,9 +378,9 @@
                 </div>
             </li>
             <li class="cartBox d-flex align-items-center">
-                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid"/></a>
+                <a href="javascript:;" class="cartBox_delete"><img src="images/delete.svg" alt="delete" class="img-fluid" /></a>
                 <div class="cartBox_img">
-                    <img src="images/review-product.png" alt="product" class="img-fluid"/>
+                    <img src="images/review-product.png" alt="product" class="img-fluid" />
                 </div>
                 <div class="cartBox_cnt">
                     <h4>1001 Nights</h4>
@@ -285,7 +388,7 @@
                     <div class="cartBox_price"><span class="finalPrice">12 AED</span> <span class="offPrice">24 AED</span></div>
                 </div>
             </li>
-        
+
         </ul>
     </div>
     <div class="cartMenu_footer">
@@ -299,8 +402,8 @@
             </div>
         </div>
         <div class="cartMenu_footer_bottom">
-            <a class="btn btn-primary d-block mb-2" href="#">Checkout</a>    
-            <a class="btn btn-primary-outline d-block" href="#">Continue shopping</a>    
+            <a class="btn btn-primary d-block mb-2" href="#">Checkout</a>
+            <a class="btn btn-primary-outline d-block" href="#">Continue shopping</a>
         </div>
     </div>
 </div>
